327c0934613dc20d8abd3296b328cc0d57fd6434
kevindantas <kevin.ds.29@gmail.com>
2016-07-21 10:29:45 -0300

    Workshop create method

--- imports/api/workshop/Workshop.js
+++ imports/api/workshop/Workshop.js
@@ -1,3 +1,23 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 export const Workshop = new Mongo.Collection('workshop');
+
+
+Meteor.methods({
+	'workshop.create'(data) {
+		if(!this.userId)
+			throw new Meteor.Error('not-authorized');
+
+		Workshop.insert({
+			title: data.title,
+			description: data.description,
+			date: data.date,
+			hour: data.hour,
+			vacancies: data.vacancies,
+			comments: [],
+			files: []
+		})
+
+	}
+})
